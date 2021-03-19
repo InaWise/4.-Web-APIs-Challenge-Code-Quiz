ceaf501d44367f8c94f094d3312406c251c99729
InaWise <77795818+InaWise@users.noreply.github.com>
2021-03-19 14:02:07 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+// GIVEN I am taking a code quiz
+// WHEN I click the start button 
+function start () {
+const startButton = document.getElementById('Start-button');
+const countDown = document.getElementById('timer-count-down')
+const nextButton = document.getElementById('next-button')
+// startButton.onclick('
+// start.countDown
+$('#StartButton').on('click', () => {
+    console.log('I was clicked');
+   $('#question-box').removeClass('hideQuestion');
+
+  });
+  
+
+    
+} 
+
+// $('.wrong-answer-one answer').on('click'), () =>
+// $('.wrong-text-one answer-text').fadeOut('fast');
+
+
+// $('.wrong-answer-two answer').on('click'), () =>
+// $('.wrong-text-two answer-text').fadeOut('fast');
+
+// $('.wrong-answer-three answer').on('click'), () =>
+// $('.wrong-text-three answer-text').fadeOut('fast');
+
+
+// $('.correct-answer-one answer').on('click'), () =>
+// $('.wrong-text-three answer-text').fadeOut('fast');
+
+
+
+
+
+
+
+
+
+
+
+
+
+// THEN a timer starts and I am presented with a question
+// WHEN I answer a question
+// THEN I am presented with another question
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+// WHEN the game is over
+// THEN I can save my initials and score
+
+// var sec = 60;
+// var time = setInterval(myTimer, 1000);
+
+// function myTimer() {
+//     document.getElementById('timer').innerHTML = sec + "sec left";
+//     sec--;
+//     if (sec == -1) {
+//         clearInterval(time);
+//         alert("Game over! ");
+//     
+// 
+start()
